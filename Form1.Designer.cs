b438268dcdb25995aae05456f872169d152b1418
pyLexxDramma <lexxdramma@vk.com>
2025-01-21 13:10:48 +0400

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace FinancialTracker
+{
+    partial class FinancialTracker
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewTransactions = new System.Windows.Forms.DataGridView();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Description = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Amount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Category = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBoxDescription = new System.Windows.Forms.TextBox();
+            this.textBoxAmount = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.labelBalance = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnIncomeExpense = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTransactions)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewTransactions
+            // 
+            this.dataGridViewTransactions.BackgroundColor = System.Drawing.SystemColors.HotTrack;
+            this.dataGridViewTransactions.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewTransactions.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.Description,
+            this.Amount,
+            this.Date,
+            this.Category});
+            this.dataGridViewTransactions.Location = new System.Drawing.Point(954, 220);
+            this.dataGridViewTransactions.Name = "dataGridViewTransactions";
+            this.dataGridViewTransactions.RowTemplate.Height = 46;
+            this.dataGridViewTransactions.Size = new System.Drawing.Size(1366, 692);
+            this.dataGridViewTransactions.TabIndex = 0;
+            // 
+            // Id
+            // 
+            this.Id.HeaderText = "Id";
+            this.Id.Name = "Id";
+            // 
+            // Description
+            // 
+            this.Description.HeaderText = "Описание";
+            this.Description.Name = "Description";
+            // 
+            // Amount
+            // 
+            this.Amount.HeaderText = "Сумма";
+            this.Amount.Name = "Amount";
+            // 
+            // Date
+            // 
+            this.Date.HeaderText = "Дата";
+            this.Date.Name = "Date";
+            // 
+            // Category
+            // 
+            this.Category.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Category.HeaderText = "Категория";
+            this.Category.Name = "Category";
+            // 
+            // textBoxDescription
+            // 
+            this.textBoxDescription.BackColor = System.Drawing.Color.Black;
+            this.textBoxDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxDescription.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.textBoxDescription.Location = new System.Drawing.Point(199, 84);
+            this.textBoxDescription.Name = "textBoxDescription";
+            this.textBoxDescription.Size = new System.Drawing.Size(563, 62);
+            this.textBoxDescription.TabIndex = 1;
+            // 
+            // textBoxAmount
+            // 
+            this.textBoxAmount.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBoxAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxAmount.Location = new System.Drawing.Point(199, 220);
+            this.textBoxAmount.Name = "textBoxAmount";
+            this.textBoxAmount.Size = new System.Drawing.Size(563, 89);
+            this.textBoxAmount.TabIndex = 2;
+            this.textBoxAmount.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.Cyan;
+            this.btnAdd.Location = new System.Drawing.Point(199, 531);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(256, 109);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Добавить";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Crimson;
+            this.btnDelete.Location = new System.Drawing.Point(199, 773);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(256, 112);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Удалить";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            // 
+            // labelBalance
+            // 
+            this.labelBalance.AutoSize = true;
+            this.labelBalance.BackColor = System.Drawing.SystemColors.Desktop;
+            this.labelBalance.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelBalance.ForeColor = System.Drawing.Color.Cornsilk;
+            this.labelBalance.Location = new System.Drawing.Point(954, 84);
+            this.labelBalance.Name = "labelBalance";
+            this.labelBalance.Size = new System.Drawing.Size(458, 82);
+            this.labelBalance.TabIndex = 7;
+            this.labelBalance.Text = "Баланс: 0.00";
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnSave.Location = new System.Drawing.Point(611, 621);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(255, 120);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Сохранить";
+            this.btnSave.UseVisualStyleBackColor = false;
+            // 
+            // btnIncomeExpense
+            // 
+            this.btnIncomeExpense.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnIncomeExpense.ForeColor = System.Drawing.Color.Cyan;
+            this.btnIncomeExpense.Location = new System.Drawing.Point(1349, 943);
+            this.btnIncomeExpense.Name = "btnIncomeExpense";
+            this.btnIncomeExpense.Size = new System.Drawing.Size(562, 98);
+            this.btnIncomeExpense.TabIndex = 9;
+            this.btnIncomeExpense.Text = "Доход/Расход";
+            this.btnIncomeExpense.UseVisualStyleBackColor = false;
+            // 
+            // FinancialTracker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(2451, 1094);
+            this.Controls.Add(this.btnIncomeExpense);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.labelBalance);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.textBoxAmount);
+            this.Controls.Add(this.textBoxDescription);
+            this.Controls.Add(this.dataGridViewTransactions);
+            this.Name = "FinancialTracker";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FinancialTracker_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTransactions)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewTransactions;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Description;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Amount;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Date;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Category;
+        private System.Windows.Forms.TextBox textBoxDescription;
+        private System.Windows.Forms.TextBox textBoxAmount;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label labelBalance;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnIncomeExpense;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO; // Для работы с файлами
+using System.Linq;
+using System.Windows.Forms;
+using OfficeOpenXml; // Для работы с EPPlus
+
+namespace FinancialTracker
+{
+    public partial class FinancialTracker : Form
+    {
+        private decimal balance = 0;
+        private List<Transaction> transactions = new List<Transaction>();
+        private int nextId = 1;
+
+        public FinancialTracker()
+        {
+            InitializeComponent();
+            ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial; // Установка контекста лицензии
+            btnAdd.Click += btnAdd_Click;
+            btnDelete.Click += btnDelete_Click;
+            btnSave.Click += btnSave_Click; // Привязка обработчика для кнопки сохранения
+            btnIncomeExpense.Click += btnIncomeExpense_Click; // Привязка обработчика для кнопки доход/расход
+        }
+
+        private void FinancialTracker_Load(object sender, EventArgs e)
+        {
+            dataGridViewTransactions.Columns.Add("Id", "Id");
+            dataGridViewTransactions.Columns.Add("Description", "Описание");
+            dataGridViewTransactions.Columns.Add("Amount", "Сумма");
+            dataGridViewTransactions.Columns.Add("Date", "Дата");
+
+            dataGridViewTransactions.Columns["Id"].ReadOnly = true; // Столбец Id - только для чтения
+            LoadTransactionsFromExcel(@"D:\Development\FinancialTracker\transactions.xlsx"); // Загрузка существующих транзакций
+            UpdateOverallBalance(); // Обновление общего баланса при загрузке
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string description = textBoxDescription.Text;
+            decimal amount;
+            if (decimal.TryParse(textBoxAmount.Text, out amount))
+            {
+                Transaction transaction = new Transaction(nextId++, description, amount, DateTime.Now);
+                transactions.Add(transaction);
+                UpdateBalance(amount);
+                UpdateDataGridView();
+                ClearInputFields();
+            }
+            else
+            {
+                MessageBox.Show("Введите корректную сумму.");
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewTransactions.SelectedRows.Count > 0)
+            {
+                int selectedId = (int)dataGridViewTransactions.SelectedRows[0].Cells["Id"].Value;
+                Transaction transactionToDelete = transactions.Find(t => t.Id == selectedId);
+
+                if (transactionToDelete != null)
+                {
+                    transactions.Remove(transactionToDelete);
+                    UpdateBalance(-transactionToDelete.Amount); // Изменение баланса
+                    UpdateDataGridView();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Выберите транзакцию для удаления.");
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveTransactionsToExcel(@"D:\Development\FinancialTracker\transactions.xlsx");
+        }
+
+        private void SaveTransactionsToExcel(string filePath)
+        {
+            FileInfo fileInfo = new FileInfo(filePath);
+            using (ExcelPackage package = new ExcelPackage(fileInfo))
+            {
+                ExcelWorksheet worksheet;
+
+                // Проверяем, есть ли уже лист в файле
+                if (package.Workbook.Worksheets.Count == 0)
+                {
+                    worksheet = package.Workbook.Worksheets.Add("Транзакции"); // Создаем новый лист, если его нет
+                }
+                else
+                {
+                    worksheet = package.Workbook.Worksheets[0]; // Получаем первый лист
+                    worksheet.Cells.Clear(); // Очищаем существующие данные
+                }
+
+                // Устанавливаем заголовки
+                worksheet.Cells[1, 1].Value = "Id";
+                worksheet.Cells[1, 2].Value = "Описание";
+                worksheet.Cells[1, 3].Value = "Сумма";
+                worksheet.Cells[1, 4].Value = "Дата";
+
+                int row = 2; // Начинаем со второй строки
+
+                foreach (Transaction transaction in transactions)
+                {
+                    worksheet.Cells[row, 1].Value = transaction.Id;
+                    worksheet.Cells[row, 2].Value = transaction.Description;
+                    worksheet.Cells[row, 3].Value = transaction.Amount;
+                    worksheet.Cells[row, 4].Value = transaction.Date;
+                    worksheet.Cells[row, 4].Style.Numberformat.Format = "dd.MM.yyyy"; // Формат даты
+                    row++;
+                }
+
+                package.Save(); // Сохраняем изменения
+            }
+            MessageBox.Show("Транзакции успешно сохранены в файл Excel.");
+        }
+
+        private void LoadTransactionsFromExcel(string filePath)
+        {
+            FileInfo fileInfo = new FileInfo(filePath);
+            if (fileInfo.Exists)
+            {
+                using (ExcelPackage package = new ExcelPackage(fileInfo))
+                {
+                    var worksheet = package.Workbook.Worksheets[0]; // Получаем первый лист
+                    int rowCount = worksheet.Dimension.Rows;
+
+                    for (int row = 2; row <= rowCount; row++) // Начинаем со второй строки
+                    {
+                        int id = int.Parse(worksheet.Cells[row, 1].Text);
+                        string description = worksheet.Cells[row, 2].Text;
+                        decimal amount = decimal.Parse(worksheet.Cells[row, 3].Text);
+                        DateTime date = DateTime.Parse(worksheet.Cells[row, 4].Text);
+
+                        transactions.Add(new Transaction(id, description, amount, date));
+                        nextId = Math.Max(nextId, id + 1); // Обновляем следующий ID
+                    }
+                }
+                UpdateDataGridView();
+            }
+        }
+
+        private void btnIncomeExpense_Click(object sender, EventArgs e)
+        {
+            decimal totalIncome = transactions.Where(t => t.Amount > 0).Sum(t => t.Amount);
+            decimal totalExpense = transactions.Where(t => t.Amount < 0).Sum(t => t.Amount);
+            decimal totalBalance = totalIncome + totalExpense;
+
+            MessageBox.Show($"Общий доход: {totalIncome:C}\nОбщий расход: {Math.Abs(totalExpense):C}\nОбщий баланс: {totalBalance:C}");
+        }
+
+        // Метод для обновления общего баланса
+        private void UpdateOverallBalance()
+        {
+            decimal totalIncome = transactions.Where(t => t.Amount > 0).Sum(t => t.Amount);
+            decimal totalExpense = transactions.Where(t => t.Amount < 0).Sum(t => t.Amount);
+            balance = totalIncome + totalExpense; // Устанавливаем текущий баланс
+            labelBalance.Text = $"Баланс: {balance:C}"; // Обновляем лейбл с балансом
+        }
+
+        private void UpdateBalance(decimal amount)
+        {
+            balance += amount;
+            labelBalance.Text = $"Баланс: {balance:C}"; // C - для форматирования валюты
+        }
+
+        private void UpdateDataGridView()
+        {
+            dataGridViewTransactions.Rows.Clear();
+            foreach (Transaction transaction in transactions)
+            {
+                dataGridViewTransactions.Rows.Add(transaction.Id, transaction.Description, transaction.Amount, transaction.Date);
+            }
+        }
+
+        private void ClearInputFields()
+        {
+            textBoxDescription.Clear();
+            textBoxAmount.Clear();
+        }
+
+        private class Transaction
+        {
+            public int Id { get; set; }
+            public string Description { get; set; }
+            public decimal Amount { get; set; }
+            public DateTime Date { get; set; }
+
+            public Transaction(int id, string description, decimal amount, DateTime date)
+            {
+                Id = id;
+                Description = description;
+                Amount = amount;
+                Date = date;
+            }
+        }
+    }
+}
+
